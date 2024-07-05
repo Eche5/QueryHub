f8a8241190e9e11e57a5dd40a7e8535a6964b03d
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-07-05 13:07:13 +0100

    base_url

--- components/Authentication/axios.jsx
+++ components/Authentication/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const BASE_URL = "https://queryhub.adaptable.app/v1/";
-const BASE_URL = "http://localhost:8080/v1/";
+const BASE_URL = "https://queryhub.adaptable.app/v1/";
+// const BASE_URL = "http://localhost:8080/v1/";
 
 export default axios.create({
   baseURL: BASE_URL,
